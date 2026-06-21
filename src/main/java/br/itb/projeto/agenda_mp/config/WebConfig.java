00e9fb5ea46eb2d0dcc4112bc578a28e3c903f66
Maycon826530 <moreiramaycon932@gmail.com>
2026-06-21 14:33:26 -0300

    AMP BE 0.6.2

--- src/main/java/br/itb/projeto/agenda_mp/config/WebConfig.java
+++ src/main/java/br/itb/projeto/agenda_mp/config/WebConfig.java
@@ -1,14 +1,16 @@
 package br.itb.projeto.agenda_mp.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-				.allowCredentials(false).maxAge(3600);
-	}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
 }
