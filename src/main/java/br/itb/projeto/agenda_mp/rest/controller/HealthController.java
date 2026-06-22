3e6bfbb3b9de7537d69c3c77e7a9f61ea13893db
Nyxalorian <moreiramaycon932@gmail.com>
2026-06-22 13:27:30 +0000

    AMP BE 0.6.3

---
+++ src/main/java/br/itb/projeto/agenda_mp/rest/controller/HealthController.java
@@ -0,0 +1,13 @@
+package br.itb.projeto.agenda_mp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
