54bb6cad3352e990bdcf5c38d16131d0e242039b
Maycon826530 <moreiramaycon932@gmail.com>
2026-06-23 21:13:11 -0300

    AMP BE 0.6.4

--- src/main/java/br/itb/projeto/agenda_mp/rest/controller/HealthController.java
+++ src/main/java/br/itb/projeto/agenda_mp/rest/controller/HealthController.java
@@ -1,4 +1,4 @@
-package br.itb.projeto.agenda_mp.controller;
+package br.itb.projeto.agenda_mp.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
